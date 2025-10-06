050f61baac913972146e0e017da84500573bf52d
awen <alduanecuevasmirasol@gmail.com>
2025-10-06 22:26:53 +0800

    updated

--- script.js
+++ script.js
@@ -2,56 +2,37 @@ const DB =
   "https://iot-temp-humidity-monito-fbbb4-default-rtdb.asia-southeast1.firebasedatabase.app";
 const PATH = "/sensor.json";
 
+const tempElem = document.getElementById("temp");
+const humElem = document.getElementById("hum");
+const lastElem = document.getElementById("last");
+
+function updateUI(temp, hum, last, color) {
+  tempElem.textContent = temp;
+  humElem.textContent = hum;
+  lastElem.textContent = last;
+  tempElem.style.color = humElem.style.color = lastElem.style.color = color;
+}
+
 async function fetchData() {
   try {
     const res = await fetch(DB + PATH);
     const data = await res.json();
 
-    const tempElem = document.getElementById("temp");
-    const humElem = document.getElementById("hum");
-    const lastElem = document.getElementById("last");
-
     const t = data?.temperature ?? data?.temp;
     const h = data?.humidity ?? data?.hum;
-
     let dt = new Date(data?.timestamp?.replace(" ", "T") + "+08:00");
 
-    const phtTime = dt.toLocaleString("en-PH", {
-      timeZone: "Asia/Manila",
-      hour12: true,
-    });
-
     if (t !== undefined && h !== undefined && !isNaN(dt)) {
-      tempElem.textContent = `${t}°C`;
-      humElem.textContent = `${h}%`;
-      lastElem.textContent = `Last update: ${phtTime}`;
-      tempElem.style.color =
-        humElem.style.color =
-        lastElem.style.color =
-          "#eeeeee";
+      const phtTime = dt.toLocaleString("en-PH", {
+        timeZone: "Asia/Manila",
+        hour12: true,
+      });
+      updateUI(`${t}°C`, `${h}%`, `Last update: ${phtTime}`, "#eeeeee");
     } else {
-      tempElem.textContent =
-        humElem.textContent =
-        lastElem.textContent =
-          "No data";
-      tempElem.style.color =
-        humElem.style.color =
-        lastElem.style.color =
-          "#d62828";
+      updateUI("No data", "No data", "No data", "#d62828");
     }
   } catch {
-    const tempElem = document.getElementById("temp");
-    const humElem = document.getElementById("hum");
-    const lastElem = document.getElementById("last");
-
-    tempElem.textContent =
-      humElem.textContent =
-      lastElem.textContent =
-        "No data";
-    tempElem.style.color =
-      humElem.style.color =
-      lastElem.style.color =
-        "#d62828";
+    updateUI("No data", "No data", "No data", "#d62828");
   }
 }
 
